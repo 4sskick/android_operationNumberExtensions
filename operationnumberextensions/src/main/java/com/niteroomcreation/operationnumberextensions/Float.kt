ac902e96c15a0a0793be0767ffeea6684255494a
Septian Adi Wijaya <developadi@gmail.com>
2020-11-10 18:45:19 +0700

    [float] is negative + unit test

--- operationnumberextensions/src/main/java/com/niteroomcreation/operationnumberextensions/Float.kt
+++ operationnumberextensions/src/main/java/com/niteroomcreation/operationnumberextensions/Float.kt
@@ -4,3 +4,53 @@ package com.niteroomcreation.operationnumberextensions
  * Created by Septian Adi Wijaya on 28/10/2020.
  * please be sure to add credential if you use people's code
  */
+
+fun Float.isNegative(): Boolean {
+    return (this < 0)
+}
+
+fun Float.isPositive(): Boolean {
+    return (this > 0)
+}
+
+/**
+ * bilangan berpangkat
+ * ex. 3^2 = 9
+ */
+fun Float.power(pow: Int): Float {
+
+    var value = 1f
+    repeat(pow) {
+        value = value * this
+    }
+
+    return value
+}
+
+fun Float.square(): Float {
+    return power(2)
+}
+
+fun Float.cubic(): Float {
+    return power(3)
+}
+
+fun Float.absolute(): Float {
+    if (this < 0)
+        return -this
+    return this
+}
+
+/**
+ * berapapun nilainya selama diatas .5 atau dibawah
+ * akan di bulatkan kebawah
+ */
+fun Float.round(): Float {
+    var value = (this + .5).toInt()
+
+    return value.toFloat()
+}
+
+fun Float.inverse(): Float {
+    return 1 / this
+}
---
+++ operationnumberextensions/src/test/java/com/niteroomcreation/operationnumberextensions/FloatKtTest.kt
@@ -0,0 +1,46 @@
+package com.niteroomcreation.operationnumberextensions
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Septian Adi Wijaya on 10/11/20
+ */
+class FloatKtTest {
+
+    @Test
+    fun isNegative() {
+        var number = -2f
+
+        assertEquals("the $number should be a negative", true, number.isNegative())
+    }
+
+    @Test
+    fun isPositive() {
+    }
+
+    @Test
+    fun power() {
+    }
+
+    @Test
+    fun square() {
+    }
+
+    @Test
+    fun cubic() {
+    }
+
+    @Test
+    fun absolute() {
+    }
+
+    @Test
+    fun round() {
+    }
+
+    @Test
+    fun inverse() {
+    }
+}
